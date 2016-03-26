f4dbaeb4beb251f6ad0cebb8107d0d141cfb7142
Alex Cummaudo <alexcu@me.com>
2016-03-26 12:30:15 +1100

    CONFIG: Add build script and improve folder structure

---
+++ run.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+#
+# Alex Cummaudo - 1744070
+# Intro to AI - Assignment 1
+# Run Script
+#
+
+EXECUTABLE="bin/puzzle-solver"
+SOURCE_CODE="src/*.swift"
+
+#
+# Check that swift 2.2 is installed
+#
+function check_swift_version() {
+  SWIFT_INSTALLED=$(which swiftc 2>&1 >/dev/null; echo $?)
+  SWIFT_VERSION_22=$(swiftc -v 2>&1 >/dev/null | grep -c 2.2)
+
+  echo "Checking your Swift compiler version..."
+  if [ $SWIFT_INSTALLED -ne 0 ] || [ $SWIFT_VERSION_22 -ne 1  ]; then
+    echo "You do not have the Swift 2.2 compiler installed on this machine"
+    echo "Please download and install Swift 2.2 from http://swift.org/download"
+    exit 1
+  else
+    echo "OK - Swift 2.2 is installed!"
+  fi
+}
+
+#
+# Compiles Swift source to exeuctable
+#
+function compile_src() {
+  echo "Compiling Swift source code..."
+  mkdir -p $(dirname $EXECUTABLE)
+  swiftc $SOURCE_CODE -o $EXECUTABLE
+  if [ $? -ne 0 ]; then
+    echo "Couldn't build my code on your machine :'("
+    echo "Please don't fail me - I tried!"
+    echo "Can you email me at acummaudo@swin.edu.au and we can work out why?"
+    exit 1
+  fi
+}
+
+#
+# Runs the executable
+#
+function run_executable() {
+  $EXECUTABLE "$@"
+}
+
+# Make sure we run wherever the shell script is
+cd "$(dirname $0)"
+# Build the executable if it is not yet build
+if [ ! -x $EXECUTABLE ]; then
+  echo "Source not yet compiled!"
+  check_swift_version
+  compile_src
+  echo "Compied! Running $EXECUTABLE"
+fi
+# Now run it
+run_executable $@
