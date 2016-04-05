6b68bc27883f1ea7942e5b1fef384753ff06dd73
Alex Cummaudo <alexcu@me.com>
2016-04-05 15:17:12 +1000

    DOC: Update documentation for Search Method observer

--- xcode/PuzzleProblem/SearchMethodObserver.swift
+++ xcode/PuzzleProblem/SearchMethodObserver.swift
@@ -10,8 +10,19 @@
 /// Observes search methods that are currently traversing and notifies all subscribers
 ///
 struct SearchMethodObserver {
+    ///
+    /// Singleton shared instance
+    ///
     static var sharedObserver = SearchMethodObserver()
+    ///
+    /// List of subscribers that the observer notifies
+    ///
     var subscribers: [SearchMethodSubscriber] = []
+    ///
+    /// Notifies all subscribers of a node traversal
+    /// - Parameter node: The node that was just traversed
+    /// - Parameter isSolved: Whether or not this node is the goal node
+    ///
     func notify(node: Node, isSolved: Bool) {
         for sub in subscribers {
             sub.didTraverseNode(node, isSolved: isSolved)
@@ -19,6 +30,19 @@ struct SearchMethodObserver {
     }
 }
 
+///
+/// A subscriber to a search method observer
+/// - Remarks: Once implemented, must add `self` to the `sharedObserver`'s subscribers
+///            array, i.e.:
+/// ```
+/// SharedMethodObserver.sharedObserver.subscribers.append(self)
+/// ```
+///
 protocol SearchMethodSubscriber {
+    ///
+    /// Notification event when a node was just traversed
+    /// - Parameter node: The node that was just traversed
+    /// - Parameter isSolved: Whether or not this node is the goal node
+    ///
     func didTraverseNode(node: Node, isSolved: Bool)
 }
