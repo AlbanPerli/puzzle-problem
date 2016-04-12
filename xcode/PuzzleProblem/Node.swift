9bca693a43be591e2d2feb258dbe3c35ae83b8ad
Alex Cummaudo <alexcu@me.com>
2016-04-12 17:20:40 +1000

    SPEED: Remove lazy evaluation to reduce memory complexity

--- xcode/PuzzleProblem/Node.swift
+++ xcode/PuzzleProblem/Node.swift
@@ -49,38 +49,38 @@ class Node: Equatable, CustomDebugStringConvertible, Hashable {
     /// - Remarks: Expensive to determine this property, which is why it is
     ///            marked with a `lazy` initialiser
     ///
-    lazy var ansecstors: [Node] = {
+    var ansecstors: [Node] {
         var ansecstors: [Node] = [self]
         // Unwrap the parent while it exists
-        while var parent = ansecstors.last?.parent {
+        while let parent = ansecstors.last?.parent {
             ansecstors.append(parent)
         }
         return ansecstors
-    }()
+    }
     
     ///
     /// The children this node has
     /// - Remarks: Expensive to determine this property, which is why it is
     ///            marked with a `lazy` initialiser
     ///
-    lazy var children: [Node] = {
+    var children: [Node] {
         return self.state.possibleActions.map { action -> Node in
             let nextState = self.state.performAction(action)
             return Node(parent: self, state: nextState)
         }
-    }()
+    }
 
     ///
     /// Determines whether the node is empty or not
     ///
-    lazy var isEmpty: Bool = {
+    var isEmpty: Bool {
         return self.children.isEmpty
-    }()
+    }
 
     ///
     /// Computes the actions that lead to this node's state
     ///
-    lazy var actionsToThisNode: [Action] = {
+    var actionsToThisNode: [Action] {
         var result: [Action] = []
         var ancestor: Node? = self.parent
         if let currentLeadingAction = self.state.leadingAction {
@@ -95,7 +95,7 @@ class Node: Equatable, CustomDebugStringConvertible, Hashable {
             ancestor = ancestor?.parent
         }
         return result.reverse()
-    }()
+    }
 
     // MARK: Initialisers for nodes
 
