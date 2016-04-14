07857e2762dbb51dce511d776fbe6c4078863c22
Alex Cummaudo <alexcu@me.com>
2016-04-14 15:15:31 +1000

    TEST: Update tests for random state validation checks

--- xcode/PuzzleSolverTests/ImpossibleStateTests.swift
+++ xcode/PuzzleSolverTests/ImpossibleStateTests.swift
@@ -11,13 +11,17 @@ import XCTest
 class ImpossibleStateTests: XCTestCase {
     // These states are impossible to solve
     func testImpossibleToSolve() {
-        XCTAssertFalse(validateSequence([6,7,4,1,5,3,8,0,2], width: 3))
-        XCTAssertFalse(validateSequence([3,4,2,1,8,7,6,0,5], width: 3))
+        XCTAssertFalse(State(sequence: [6,7,4,1,5,3,8,0,2], height: 3, width: 3).isValid)
+        XCTAssertFalse(State(sequence: [3,4,2,1,8,7,6,0,5], height: 3, width: 3).isValid)
+        XCTAssertFalse(State(sequence: [4,5,0,1,3,2],       height: 2, width: 3).isValid)
+        XCTAssertFalse(State(sequence: [0,7,2,1,4,6,3,5],   height: 2, width: 4).isValid)
+        XCTAssertFalse(randomState(5, cols: 5, isValid: false).isValid)
     }
 
     // These states are possible to solve
     func testPossibleToSolve() {
-        XCTAssertTrue(validateSequence([6,3,1,5,0,2,7,4,8], width: 3))
-        XCTAssertTrue(validateSequence([3,4,1,6,0,2,5,7,8], width: 3))
+        XCTAssertTrue(State(sequence: [6,3,1,5,0,2,7,4,8], height: 3, width: 3).isValid)
+        XCTAssertTrue(State(sequence: [3,4,1,6,0,2,5,7,8], height: 3, width: 3).isValid)
+        XCTAssertTrue(randomState(5, cols: 5, isValid: true).isValid)
     }
 }
